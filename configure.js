b8e4e4241df4fdfedc25c8b697903537506859a3
Erico_@_tush <33453574+ertush@users.noreply.github.com>
2018-07-25 20:46:12 +0300

    Add files via upload
    Added imgPloadr project files

---
+++ configure.js
@@ -0,0 +1,41 @@
+var path = require('path'),
+ routes = require('./routes'),
+ exphbs = require('express-handlebars'),
+ express = require('express'),
+ bodyParser = require('body-parser'),
+ cookieParser = require('cookie-parser'),
+ morgan = require('morgan'),
+ methodOverride = require('method-override'),
+ errorHandler = require('errorhandler'),
+ multer = require('multer'),
+ moment = require('moment');
+ module.exports = function(app) {
+
+ app.use(morgan('dev'));
+ app.use(multer({ dest: path.join(__dirname,
+ 'public/upload/temp')}));
+ //app.use(bodyParser.urlencoded({'extended':true}));
+ //app.use(bodyParser.json());
+ app.use(methodOverride());
+ app.use(cookieParser('some-secret-value-here'));
+ routes(app);//moving the routes to routes folder.
+ app.use('/public/', express.static(path.join(__dirname,'public')));
+if ('development' === app.get('env')) {
+ app.use(errorHandler());
+}
+ routes(app);
+
+ app.engine('handlebars', exphbs.create({
+ defaultLayout: 'main',
+ layoutsDir: app.get('views') + '/layouts',
+ partialsDir: [app.get('views') + '/partials'],
+ helpers: {
+ timeago: function(timestamp) {
+ return moment(timestamp).startOf('minute').fromNow();
+ }
+ }
+}).engine);
+app.set('view engine', 'handlebars');
+ 
+return app;
+};
